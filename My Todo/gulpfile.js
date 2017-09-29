32a234e32006e4f81e0a4bc2510d4b35f6a34429
ManuelGallegos <manuel_mty@yahoo.com>
2017-09-29 18:06:00 -0400

    Add project files.

---
+++ My Todo/gulpfile.js
@@ -0,0 +1,24 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+var paths = {
+    sourcePath: "./node_modules",
+    targetPath: "./wwwroot/libs"
+};
+var librariesToMove = [
+    paths.sourcePath + '/core-js/client/shim.min.js',
+    paths.sourcePath + '/zone.js/dist/zone.min.js',
+    paths.sourcePath + '/systemjs/dist/system.src.js'
+];
+
+gulp.task('default', function () {
+   
+});
+
+gulp.task('librariesToMove', function () {
+    return gulp.src(librariesToMove).pipe(gulp.dest(paths.targetPath));
+});
